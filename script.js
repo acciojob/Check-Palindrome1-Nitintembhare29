cd6d845b6c04f0190360a44c028e8ffcaed751d4
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-01 00:06:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 // complete the given function
 
-function palindrome(s){
-// 	str.trim();
-// 	str.toLowerCase();
-// for(var i=0; i<str.length/2; i++){
-// 	if(str.charAt(i)!=str.charAt(str.length-1-i)){
-// 		return false;
-// 	}
+function palindrome(str){
+	str.trim();
+	str.toLowerCase();
+for(var i=0; i<str.length/2; i++){
+	if(str.charAt(i)!=str.charAt(str.length-1-i)){
+		return false;
+	}
  
-// 	}
-// return true;
-
-	  s = s.lower() # convert all characters to lowercase
-    s = ''.join(c for c in s if c.isalnum()) # remove non-alphanumeric characters
-    return s == s[::-1] # check if the string is equal to its reverse
-
+	}
+return true;
 }
 module.exports = palindrome
