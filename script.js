65e12fe7a2092ccd8cf2c47bb44bb111aa819955
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-03-31 20:55:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // complete the given function
 
 function palindrome(str){
+	str.toLowerCase();
 for(var i=0; i<str.length/2; i++){
 	if(str.charAt(i)!=str.charAt(str.length-1-i)){
 		return false;
